ebb484f468d72b791eaecfb1c90b2b20e2ef87f0
Varga-Tóth Máté <vargatothmate@gmail.com>
2026-04-15 14:50:53 +0200

    Initial commit

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,12 @@
+<section class="relative scale-[1.05] lg:block hidden overflow-hidden">
+  <img
+    src="assets/HERO@2x.png"
+    alt="Nyeremények, hero kép banner"
+    class="w-full scale-[1.07] -translate-x-8 -translate-y-12"
+  />
+  <h1 class="sr-only">Minden falat jókedvvel töltve!</h1>
+</section>
+<section class="lg:hidden">
+  <img src="assets/hero_mobile.png" alt="hero kép mobil" class="w-full" />
+  <h1 class="sr-only">Minden falat jókedvvel töltve!</h1>
+</section>
---
+++ src/lib/components/JatekIdotartama.svelte
@@ -0,0 +1,82 @@
+<section class="relative bg-[#ffe6c7] rounded-t-3xl lg:-mt-[17vw]">
+  <img
+    src="assets/BG pattern.svg"
+    alt="Barack háttér"
+    class="w-full z-1 scale-[1.05] absolute top-0 left-0 pointer-events-none"
+  />
+  <h2 class="markerfield lg:text-[4vw] text-[8vw] text-center leading-[3vw] pt-[6vw] pb-[3vw]">
+    A PROMÓCIÓ IDŐTARTAMA
+  </h2>
+  <div class="relative mb-[1vw]">
+    <img
+      src="assets/3 db színes téglalap_játék időtartama.svg"
+      alt="színes négyzetek"
+      class="lg:block hidden w-[60vw] mx-auto"
+    />
+    <p
+      class="lg:absolute lg:left-[50%] lg:-translate-x-[50%] lg:top-0 text-center markerfield lg:text-[4vw] text-[8vw]"
+    >
+      2026. 04. 30. - 05. 27.
+    </p>
+    <img
+      src="assets/Tibi csoki jobb_játék időtartam@2x.png"
+      alt="Tibi csoki"
+      class="absolute -top-[7vw] left-[20vw] w-[8vw] lg:block hidden"
+    />
+  </div>
+
+  <h2
+    class="markerfield lg:text-[4vw] text-[6vw] relative z-9 lg:w-[24vw] w-[50vw] mx-auto rounded-xl text-center lg:leading-[3.5vw] leading-[6vw] lg:py-[2vw] py-[4vw] bg-[#b4cc06] lg:mt-[5vw] mt-[8vw]"
+  >
+    <span class="lg:text-[8vw] text-[16vw]">HOGYAN</span> <br />
+    JÁTSZHATSZ?
+  </h2>
+  <p
+    class="bg-white rounded-xl z-1 -mt-[1vw] text-center w-[70vw] mx-auto px-[3vw] py-[2vw] relative"
+  >
+    Vásárolj legalább 1 db a promócióban részt vevő
+    <b>tibi vagy Roshen terméket a LIDL magyarországi üzleteiben április 30. és május 27. között,</b
+    >
+    küldd be az AP kódot, a blokkot, a vásárlás pontos időpontját és az adataid, így már részt is veszel
+    <b>a napi és a heti nyeremények, valamint a fődíj, az 500.000 Ft értékű LIDL Ajándékutalvány</b>
+    sorsolásán is! <br /><br /><b>A blokkot meg kell őrizni a játék lezárultát követő 30 napig!</b>
+  </p>
+  <!-- képek -->
+  <img src="assets/Kék nyíl.svg" alt="kék nyíl" class="absolute top-[19vw] left-[16vw] w-[10vw]" />
+  <img src="assets/Zöld nyíl.svg" alt="Zöld nyíl" class="absolute top-[20vw] left-[8vw] w-[11vw]" />
+  <img
+    src="assets/narancs-kor.svg"
+    alt="narancssárga kör"
+    class="absolute top-[30vw] left-[8vw] w-[10vw]"
+  />
+  <img
+    src="assets/BLOKK_játék időtartam@2x.png"
+    alt="blokk"
+    class="absolute top-[30vw] left-[8vw] w-[10vw] z-9"
+  />
+  <img
+    src="assets/Piros nyíl.svg"
+    alt="piros nyíl"
+    class="absolute top-[45vw] left-[2vw] w-[10vw]"
+  />
+  <img
+    src="assets/Piris kis Roshen csoki jobb_játék időtartama@2x.png"
+    alt="roshen csoki"
+    class="absolute top-[10vw] -right-[2vw] w-[10vw]"
+  />
+  <img
+    src="assets/Világoskék nyíl.svg"
+    alt="kék nyíl"
+    class="absolute top-[24vw] right-[3vw] w-[12vw]"
+  />
+  <img
+    src="assets/Tibi figura_hogyan játszhatsz.svg"
+    alt="Tibi figure"
+    class="absolute top-[20vw] right-[24vw] w-[14vw]"
+  />
+  <img
+    src="assets/szines-korok.svg"
+    alt="Színes körök"
+    class="absolute top-[42vw] right-[8vw] z-11 w-[18vw]"
+  />
+</section>
